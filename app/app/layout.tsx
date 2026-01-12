5d66e82e18e050f0e514ddd1e8ccc1105de2883b
bconti123 <bryantcconti@gmail.com>
2026-01-11 18:33:13 -0800

    feat(layout): Add Home Link

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,8 +1,10 @@
 import { LogoutButton } from "@/components/LogoutButton";
+import Link from "next/link";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <h3><Link href="/app">Home</Link></h3>
       <LogoutButton />
       {children}
     </div>
